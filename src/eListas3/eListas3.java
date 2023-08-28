b6e87a725813ed169b23eb9744820830b81779fa
Piru <Piru@192.168.1.38>
2023-08-28 12:27:39 -0300

    ejercicio3

---
+++ src/eListas3/eListas3.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eListas3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Piru
+ */
+public class eListas3 {
+    public static void main(String [] args){
+     Scanner scr = new Scanner(System.in);
+     ArrayList <Jugador> jugadores= new ArrayList();
+     Jugador datos= new Jugador();
+     for (int i=0; i<5;i++){
+        System.out.println("Ingrese nombre del jugador: ");
+        datos.setNombre(scr.next());
+        System.out.println("Ingrese apellido del jugador: ");
+        datos.setApellido(scr.next());
+        System.out.println("Ingrese edad del jugador: ");
+        datos.setEdad(scr.nextInt());
+        System.out.println("Ingrese goles del jugador: ");
+        datos.setGoles(scr.nextInt());
+        jugadores.add(datos);
+                }
+    }
+    
+}
